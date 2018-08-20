3e320792f8b65b2f0946832dbb924016f634d882
Felipe Lucas <felipe.lucas@dextra-sw.com>
2018-08-19 22:48:23 -0300

    Store upload test working :)

---
+++ src/test/java/com/felipelucas/test/commons/IntegrationTestBase.java
@@ -0,0 +1,30 @@
+package com.johndeere.test.commons;
+
+import com.felipelucas.Application;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public abstract class IntegrationTestBase {
+
+    @Value("${local.server.port}")
+    private Integer port;
+
+    public String getBaseUrl(){
+        return String.format("http://localhost:%d/api", port);
+    }
+
+    public HttpEntity getHeaders(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        return new HttpEntity<>(headers);
+    }
+}
---
+++ src/test/java/com/felipelucas/test/commons/RestRequest.java
@@ -0,0 +1,96 @@
+package com.johndeere.test.commons;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+
+public class RestRequest<E> {
+
+    private String baseUrl = "";
+    private String endpoint = "";
+    private HttpMethod method;
+    private HttpHeaders headers = new HttpHeaders();
+    private E payload;
+
+    private RestRequest() {
+        this.headers = new HttpHeaders();
+    }
+
+    public static RestRequest build() {
+        return new RestRequest();
+    }
+
+    public RestRequest baseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+        return this;
+    }
+
+    public RestRequest method(HttpMethod method) {
+        this.method = method;
+        return this;
+    }
+
+    public RestRequest endpoint(String endpoint) {
+        this.endpoint = endpoint;
+        return this;
+    }
+
+    public RestRequest payload(E payload) {
+        this.payload = payload;
+        return this;
+    }
+
+
+    public ResponseEntity execute() {
+        HttpEntity entity = this.payload == null ? new HttpEntity(this.headers) : new HttpEntity(this.payload, this.headers);
+        String requestUrl = this.baseUrl + this.endpoint;
+
+        return RestTemplateBuilder.build().exchange(requestUrl, this.method, entity, new ParameterizedTypeReference<E>() {}, new Object[0]);
+    }
+
+    public <E> ResponseEntity<E> execute(Class<E> responseType) {
+        HttpEntity entity = this.payload == null ? new HttpEntity(this.headers) : new HttpEntity(this.payload, this.headers);
+        String requestUrl = this.baseUrl + this.endpoint;
+        try {
+            return RestTemplateBuilder.build()
+                    .exchange(
+                            requestUrl,
+                            this.method, entity, responseType, new Object[0]);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public <C> ResponseEntity<C> execute(ParameterizedTypeReference<C> responseType) {
+        HttpEntity entity = this.payload == null ? new HttpEntity(this.headers) : new HttpEntity(this.payload, this.headers);
+        String requestUrl = this.baseUrl + this.endpoint;
+
+        return RestTemplateBuilder.build().exchange(requestUrl, this.method, entity, responseType, new Object[0]);
+    }
+
+    public <C> ResponseEntity<C> execute(ParameterizedTypeReference<C> responseType, File file) {
+        LinkedMultiValueMap<String, Object> multipart = new LinkedMultiValueMap<>();
+        multipart.add("file", new FileSystemResource(file));
+
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity =
+                new HttpEntity<>(multipart, headers);
+
+        return RestTemplateBuilder.build().exchange(
+                baseUrl + endpoint,
+                HttpMethod.POST,
+                requestEntity,
+                responseType);
+    }
+
+}
